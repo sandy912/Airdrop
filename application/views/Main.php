977115449fbc91f48294f6a8ba04737009ecc948
isanthoshsandy <isanthoshsandy@gmail.com>
2018-05-15 03:07:18 -0400

    Added submenu

--- application/views/Main.php
+++ application/views/Main.php
@@ -1,7 +1,7 @@
 <?php $this->load->view('header'); ?>
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img alt="brand" src="<?php echo base_url(); ?>/img/fireflycoin.png" />Firefly Coin</a>
+        <a class="navbar-brand js-scroll-trigger" href="/#page-top"><img alt="brand" src="<?php echo base_url(); ?>/img/fireflycoin.png" />Firefly Coin</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <i class="fa fa-bars"></i>
         </button>
@@ -18,6 +18,14 @@
             </li>
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fa fa-user-circle-o"></i></a>
+              <ul class="submenu">
+                <li class="nav-item"><a class="nav-link" href="/user/user-profile">Dashboard</a></li>
+                <li class="nav-item"><a class="nav-link" href="/user/sign-in">Sign-in</a></li>
+                <li class="nav-item"><a class="nav-link" href="/user/register">Register</a></li>
+              </ul>
             </li>
           </ul>
         </div>
